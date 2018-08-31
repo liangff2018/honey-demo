7b8b4c95ce247c746ef68c938d06a746cc10ac75
liangff2018 <105751335@qq.com>
2018-08-31 10:54:41 +0800

    添加组织的图标

--- src/app/components/portal/index/index.component.ts
+++ src/app/components/portal/index/index.component.ts
@@ -27,22 +27,14 @@ export class IndexComponent implements OnInit {
 
   /*监听窗口改变事件*/
   @HostListener("window:resize") onResize() {
-    /*
-      //动态改变样式
-      <h3>内容214</h3>
-      <div id="contentContainer" [ngStyle]="contentStyle">
-        <app-organization ></app-organization>
-      </div>*/
     this.contentStyle = {
       "height": this.el.nativeElement.offsetParent.clientHeight-160+"px"
-      //"overflow-y": "auto"
     };
   }
 
   @HostListener("window:load") onLoad() {
     this.contentStyle = {
       "height": this.el.nativeElement.offsetParent.clientHeight-160+"px"
-      //"overflow-y": "auto"
     };
   }
 
